72369ae07ee628edcaf003017f26c35d22441341
craig <craig@DESKTOP-S3EF28S>
2021-08-14 21:14:23 +0200

    Add project files.

---
+++ FlyoutBackgroundImageTest/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace FlyoutBackgroundImageTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
